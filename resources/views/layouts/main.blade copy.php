3722ee6cf41052f485b28719797516b004c7f3d6
gabriela.debora <gabriela.debora@pmc.corp>
2022-07-20 14:59:47 -0300

    novo

--- resources/views/layouts/main.blade copy.php
+++ resources/views/layouts/main.blade copy.php
@@ -8,7 +8,6 @@
         <title>@yield('title')</title>
         <!-- Fonte do Google -->
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet"> 
-        <link rel="shortcut icon" href="{{ url('') }}/images/icon.webp">
 
         <!-- CSS Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">

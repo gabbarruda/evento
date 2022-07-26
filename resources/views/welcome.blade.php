496cbbf09ce0aa23acebc70626ce30674d6c0c21
gabriela.debora <gabriela.debora@pmc.corp>
2022-07-26 14:59:21 -0300

    novo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,14 @@
 @section('title', 'HDC Evento')
 
 @section('content')
+<div class="search"><h1>Busque um evento</h1>
+  <form action="{{ url('') }}/img/codig.jpg" method="GET">
+      <input type="text" id="search" name="search" class="form-control" placeholder="Procurar..."> 
+  </form></div>
 <div class="box-search">
  
     <div id="search-container" class="carousel slide" data-bs-ride="carousel">
-      
+  
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img src="{{ url('') }}/img/codig.jpg"  class="d-block w-100" alt="..."> 
@@ -15,7 +19,7 @@
           <img src="{{ url('') }}/img/event.jpg"  class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="...">
+          <img src="{{ url('') }}/img/codig.jpg" class="d-block w-100" alt="...">
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -27,10 +31,7 @@
         <span class="visually-hidden">Next</span>
       </button>
     </div>
-    <div class="search"><h1>Busque um evento</h1>
-      <form action="{{ url('') }}/" method="GET">
-          <input type="text" id="search" name="search" class="form-control" placeholder="Procurar..."> 
-      </form></div>
+   
 </div>
 
     

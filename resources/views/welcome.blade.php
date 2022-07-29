bd8724888cf8e2ef2be42bf3c16442d8c81259c4
gabriela.debora <gabriela.debora@pmc.corp>
2022-07-29 09:22:48 -0300

    novo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,8 @@
 @section('title', 'HDC Evento')
 
 @section('content')
-<div class="search"><h1>Busque um evento</h1>
-  <form action="{{ url('') }}/img/codig.jpg" method="GET">
-      <input type="text" id="search" name="search" class="form-control" placeholder="Procurar..."> 
-  </form></div>
 <div class="box-search">
- 
     <div id="search-container" class="carousel slide" data-bs-ride="carousel">
-  
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img src="{{ url('') }}/img/teatro.jpg"  class="d-block w-100" alt=""> 
@@ -21,6 +15,10 @@
         <div class="carousel-item">
           <img src="{{ url('') }}/img/teatro.jpg" class="d-block w-100" alt="">
         </div>
+        <div class="search"><h1>Busque um evento</h1>
+          <form action="{{ url('') }}/img/codig.jpg" method="GET">
+              <input type="text" id="search" name="search" class="form-control" placeholder="Procurar..."> 
+          </form></div>
       </div>
       {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
